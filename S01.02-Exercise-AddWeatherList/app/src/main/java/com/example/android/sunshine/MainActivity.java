9c2b23f435f8912ca1c781edf2591815fc6d745b
Zach Hall <zach.hall@syncbak.com>
2018-11-12 17:00:14 -0600

    S01.01-.02

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,24 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+
+    TextView weatherDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        weatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        String[] weatherData = new String[]{"Fake1", "Fake2", "Fake3"};
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        for(String data : weatherData){
+            weatherDisplay.append(data + "\n\n\n");
+        }
     }
 }
